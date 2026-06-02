d24a8da66103b29f805b17b7e17463aac965707f
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-06-02 14:41:12 +0400

    initial commet

--- trainee-app/src/main/java/com/example/trainee_app/HelloController.java
+++ trainee-app/src/main/java/com/example/trainee_app/HelloController.java
@@ -1,9 +1,19 @@
 package com.example.trainee_app;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-    return "Welcome to code "
 
+
+    @GetMapping("hello")
+    public String welcomeMessage() {
+        return "Welcome to Code Creators Spring Boot Training!";
+    }
+
+    @GetMapping("help")
+    public String helpMessage() {
+        return "If you are stuck, please post your error screenshot on Slack!";
+    }
 }
